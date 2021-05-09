f1922b1889cb84baf4e36a74e956dd5b326c7eab
MrMoustach <bossissamos@gmail.com>
2021-05-09 16:04:55 +0000

    cleaned up

---
+++ srcs/images/nginx/config.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+apk update 
+apk add lighttpd nginx php7-common php7-mysqli php7-imap php7-cgi fcgi php7-mbstring php7-cgi
+apk add --no-cache openssh-server openssl openrc git rsync \
+
+mkdir -p /run/openrc \
+touch /run/openrc/softlevel 
+
+mkdir -p /run/nginx
+
+openrc default 
+rc-update
+
+openssl req -x509 -nodes -days 365 -subj "/C=CA/ST=QC/O=Company, Inc./CN=mydomain.com" -addext "subjectAltName=DNS:mydomain.com" -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt;
+
+mkdir -p /data/www
+wget https://gist.githubusercontent.com/ZiKT1229/5935a10ce818ea7b851ea85ecf55b4da/raw/cd1119f65876d1b5db705ea2dc9097c5f5694ba5/snake.html
+mv snake.html /data/www/index.html
--- start.sh
+++ start.sh
@@ -1,23 +1,73 @@
+#!/bin/zsh
+echo "-------------Starting deployment-------------"
+echo '
+  __ _
+ / _| |_      ___  ___ _ ____   _____ _ __
+| |_| __|____/ __|/ _ \ '__\ \ / / _ \ '__|
+|  _| ||_____\__ \  __/ |   \ V /  __/ |
+|_|  \__|    |___/\___|_|    \_/ \___|_|'
+echo "- \e[32mSetting up minikube\e[39m"
 minikube delete
-minikube start
+minikube start --driver=virtualbox
 eval $(minikube docker-env)
+echo "- \e[32msetting up files\e[39m"
 
-docker build srcs/images/phpmyadmin -t phpmyadmin
-docker build srcs/images/wordpress -t wp
-docker build srcs/images/mysql -t mysql
-docker build srcs/images/nginx -t nginx
+export HTTP_IP="192.168.99.105"
+echo "HTTP_IP is \e[93$HTTP_IP\e[39"
 
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+sed "s/HTTP_IP/$HTTP_IP/g" srcs/deployment/metallb/metallb_sample.yaml > srcs/deployment/metallb/metallb.yaml
+sed "s/HTTP_IP/$HTTP_IP/g" srcs/images/mysql/wordpress_sample.sql > srcs/images/mysql/wordpress.sql
+sed "s/HTTP_IP/$HTTP_IP/g" srcs/images/nginx/default_sample.conf > srcs/images/nginx/default.conf
+echo "- \e[32mBuilding images\e[39m"
 
+docker build srcs/images/phpmyadmin -t phpmyadmin > /dev/null
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo "\e[32mPhpmyadmin is built\e[39m"
+else
+  echo "\e[31Building phpmyadmin failed\e[39m"
+fi
 
+docker build srcs/images/wordpress -t wp > /dev/null
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo "\e[32mWordpress is built\e[39m"
+else
+  echo "\e[31Building Wordpress failed\e[39m"
+fi
 
+docker build srcs/images/mysql -t mysql > /dev/null
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo "\e[32mMysql is built\e[39m"
+else
+  echo "\e[31Building Mysql failed\e[39m"
+fi
 
+docker build srcs/images/nginx -t nginx > /dev/null
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo "\e[32mNginx is built\e[39m"
+else
+  echo "\e[31Building Nginx failed\e[39m"
+fi
 
-kubectl apply -f srcs/deployment/metallb/metallb.yaml
-kubectl apply -f srcs/deployment/wordpress/wordpress.yaml
-kubectl apply -f srcs/deployment/phpmyadmin/phpmyadmin.yaml
-kubectl apply -f srcs/deployment/mysql/mysql.yaml
-kubectl apply -f srcs/deployment/nginx/nginx.yaml
+echo "- \e[32mSetting up Metal-lb\e[39m"
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml > /dev/null
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml > /dev/null
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)" > /dev/null
+
+echo "- \e[32mDeploying YAML files\e[39m"
+
+kubectl apply -f srcs/deployment/metallb/metallb.yaml > /dev/null 
+kubectl apply -f srcs/deployment/wordpress/wordpress.yaml > /dev/null
+kubectl apply -f srcs/deployment/phpmyadmin/phpmyadmin.yaml > /dev/null
+kubectl apply -f srcs/deployment/mysql/mysql.yaml > /dev/null
+kubectl apply -f srcs/deployment/nginx/nginx.yaml > /dev/null
+echo "- \e[32mFinishing up and starting dashboard\e[39m"
+
 minikube dashboard
+echo "- \e[32mDeleting created files\e[39m"
+
+rm srcs/deployment/metallb/metallb.yaml srcs/images/mysql/wordpress.sql srcs/images/nginx/default.conf > /dev/null
