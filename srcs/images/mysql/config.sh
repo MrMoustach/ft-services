005b82cca8bfc704d0a62e1d0add144a6af673e2
MrMoustach <bossissamos@gmail.com>
2021-05-20 19:09:23 +0100

    idk what's up

--- srcs/images/mysql/config.sh
+++ srcs/images/mysql/config.sh
@@ -13,10 +13,7 @@ chown -R mysql: /var/lib/mysql
 service mariadb stop
 service mariadb setup
 service mariadb restart
-echo "creating db"
-mysql -u root -e "CREATE USER 'iharchi'@'%' IDENTIFIED BY 'wppassword';"
-mysql -u root -e "CREATE DATABASE wordpress; use wordpress; source /temp/wordpress.sql;SELECT * from user"
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'iharchi'@'%'; FLUSH PRIVILEGES;"
+
 
 apk add libc6-compat
 
--- srcs/images/mysql/start.sh
+++ srcs/images/mysql/start.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-
-./telegraf-1.18.2/usr/bin/telegraf --config /telegraf-1.18.2/etc/telegraf/telegraf.conf &
+service mariadb setup
 service mariadb restart
+echo "creating db"
+mysql -u root -e "CREATE USER 'iharchi'@'%' IDENTIFIED BY 'wppassword';"
+mysql -u root -e "CREATE DATABASE wordpress; use wordpress; source /temp/wordpress.sql;SELECT * from user"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'iharchi'@'%'; FLUSH PRIVILEGES;"
+./telegraf-1.18.2/usr/bin/telegraf --config /telegraf-1.18.2/etc/telegraf/telegraf.conf &
 
 top
